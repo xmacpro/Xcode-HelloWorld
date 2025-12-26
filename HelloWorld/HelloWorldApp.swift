98d59a973fd2c75ec3b1baba8ed7bbd809d0c532
xmacpro <xmacpro25@proton.me>
2025-12-26 20:17:48 +0100

    Add files via upload

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by core on 26/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
